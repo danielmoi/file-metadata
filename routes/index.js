518e90d9c67b241813002a77f68610862bee0d0b
danielmoi <daniel@ampersandmoi.com>
2017-06-25 15:38:29 +1000

    Render form

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/health', (req, res) => {
   res.send('OK');
 });
 
+router.get('/', (req, res) => {
+  res.render('upload', {
+    title: 'Upload',
+  });
+});
+
 module.exports = router;
