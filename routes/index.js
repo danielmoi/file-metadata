0a2ae0a2ac00fa54467e8d4bd71cf540f0175529
danielmoi <daniel@ampersandmoi.com>
2017-06-25 16:01:34 +1000

    Return metadata

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const multer = require('multer');
+
+const upload = multer();
 
 const router = express.Router();
 
@@ -6,7 +9,17 @@ router.get('/health', (req, res) => {
   res.send('OK');
 });
 
-router.get('/', (req, res) => {
+router.post('/upload', upload.single('magic'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+    originalname,
+    mimetype,
+    size,
+  });
+});
+
+router.get('/*', (req, res) => {
   res.render('upload', {
     title: 'Upload',
   });
